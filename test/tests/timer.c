0889b213e99538617a430c816721548186eeaddd
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2015-02-25 09:13:35 +0100

    Added default value tests for Timer init methods

--- test/tests/timer.c
+++ test/tests/timer.c
@@ -11,3 +11,16 @@ void expect_init_called_with_default() {
     TIMER_Init_Expect(TIMER0, &timer_init);
 
 }
+
+void expect_init_cc_called_with_default() {
+
+    static TIMER_InitCC_TypeDef timer_init_cc = TIMER_INITCC_DEFAULT;
+    TIMER_InitCC_Expect(TIMER0, 0, &timer_init_cc);
+
+}
+
+void expect_init_dti_called_with_default() {
+
+    static TIMER_InitDTI_TypeDef timer_init_dti = TIMER_INITDTI_DEFAULT;
+    TIMER_InitDTI_Expect(TIMER0, &timer_init_dti);
+}
--- test/tests/timer.rs
+++ test/tests/timer.rs
@@ -15,11 +15,29 @@ fn test_init_called_with_default() {
     timer.init(&Default::default());
 }
 
+fn test_init_cc_called_with_default() {
+
+    unsafe { expect_init_cc_called_with_default(); }
+
+    let timer = timer::Timer::timer0();
+    timer.init_cc(0, &Default::default());
+}
+
+fn test_init_dti_called_with_default() {
+
+    unsafe { expect_init_dti_called_with_default(); }
+
+    let timer = timer::Timer::timer0();
+    timer.init_dti(&Default::default());
+}
+
 pub fn run_tests() {
     let usart1 = usart::Usart::usart1();
 
-    let tests: [fn(); 1] = [
+    let tests: [fn(); 3] = [
         test_init_called_with_default,
+        test_init_cc_called_with_default,
+        test_init_dti_called_with_default,
     ];
 
     for test in tests.iter() {
@@ -39,4 +57,6 @@ extern {
     fn Mockem_timer_Verify();
 
     fn expect_init_called_with_default();
+    fn expect_init_cc_called_with_default();
+    fn expect_init_dti_called_with_default();
 }
