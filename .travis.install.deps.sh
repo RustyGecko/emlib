44fc056d55638c65a67857ad551b8fa5badde74f
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-04-29 14:39:14 +0200

    Add own script to install travis dependencies

---
+++ .travis.install.deps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ "${TRAVIS_OS_NAME}" = "osx" ] || [ "${PLATFORM}" = "mac" ] || [ "`uname`" = "Darwin" ]; then
+    target=apple-darwin
+elif [ "${OS}" = "Windows_NT" ] || [ "${PLATFORM}" = "win" ]; then
+    windows=1
+else
+    target=unknown-linux-gnu
+fi
+
+if [ "${TRAVIS}" = "true" ] && [ "${target}" = "unknown-linux-gnu" ]; then
+    # Install gcc-arm-none-eabi for linux
+    sudo add-apt-repository ppa:terry.guo/gcc-arm-embedded -y
+    sudo apt-get update -q
+    sudo apt-get install gcc-arm-none-eabi
+fi
+
+# Install cargo-linkargs in same directory
+git clone https://github.com/RustyGecko/cargo-linkargs
+cd cargo-linkargs
+cargo build
+
+set +x
